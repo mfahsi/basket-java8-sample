4215e74edc35cd5d5993ff27e3424352e681f036
mohammed fahsi <mohammed fahsi>
2016-04-16 11:25:14 +0200

    project template

---
+++ basketpricer/src/main/java/com/mfa/samples/App.java
@@ -0,0 +1,13 @@
+package com.mfa.samples;
+
+/**
+ * Main app
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
